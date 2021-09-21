ee1a19841c1c6349dd813045e1c549e04cd05e11
snagase <snagase@ecc.ac.jp>
2021-09-21 17:30:03 +0900

    マス関連
    マス関連作成中。まだひな形も作成できてない。

---
+++ Assets/Scripts/MasuFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace dkproj
+{
+    public abstract class MasuFactory
+    {
+        public MasuData Create(Transform parent)
+        {
+            MasuData masuData = CreateMasuData();
+
+            return masuData;
+        }
+
+        protected abstract MasuData CreateMasuData();
+    }
+
+
+}
---
+++ Assets/Scripts/RelicsData.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace dkproj
+{
+    /// <summary>
+    /// 遺物のタイプ
+    /// </summary>
+    public enum eRelicsType { 
+    
+        None = -1,
+        
+        /// <summary>
+        /// ダンジョンコア
+        /// </summary>
+        DungeonCore,
+
+        /// <summary>
+        /// ダンジョンの入り口
+        /// </summary>
+        DungeonEntrance,
+
+        /// <summary>
+        /// マナ鉱脈
+        /// </summary>
+        ManaVein,
+
+        /// <summary>
+        /// 金鉱山
+        /// </summary>
+        GoldMine,
+
+        /// <summary>
+        /// ポータル
+        /// </summary>
+        Portal
+    }
+
+    /// <summary>
+    /// 遺物データ
+    /// </summary>
+    public class RelicsData : MasuData
+    {
+        [SerializeField]
+        private eRelicsType relicsType_;
+
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
