d47085123a0e202b7f0d861f11d796cf3fbb075d
snagase <snagase@ecc.ac.jp>
2021-09-28 22:52:10 +0900

    Character Prototype

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -28,36 +28,60 @@ namespace dkproj {
     /// キャラクター
     /// </summary>
     public class Character : MonoBehaviour {
+
         [Serializable]
         public class CharacterData {
 
-            public string identifier_;
+            // 識別名
+            public string m_identifier;
+
+            // 種族名
+            public string m_typeName;
             
-            public string name_;
+            // ユニーク名
+            public string m_uniqueName;
 
-            public float defalutMaxHealth_;
+            // 最大ヘルス
+            public float m_defalutMaxHealth;
 
-            public float defaultMoveSpeed_;
+            // 移動速度
+            public float m_defaultMoveSpeed;
 
-            public float defaultAttackSpeed_;
+            // 攻撃速度
+            public float m_defaultAttackSpeed;
 
             // true...移動可能 
-            public bool moveFlag_;
+            public bool m_moveFlag;
 
             // true...採掘可能
-            public bool mineFlag_;
+            public bool m_mineFlag;
 
             // true...範囲攻撃可能
-            public bool areaAttackFlag_;
+            public bool m_areaAttackFlag;
 
             // true...食事必要
-            public bool requireEatFlag_;
+            public bool m_requireEatFlag;
 
             // true...睡眠必要
-            public bool requireSleepFlag_;
+            public bool m_requireSleepFlag;
 
-            
+            // true...死亡
+            public bool m_dead = false;
         }
+
+        protected SpriteRenderer m_spriteRenderer;
+        protected Collider2D m_collider;
+        protected Animator m_animator;
+
+        protected Transform m_target;
+
+        protected readonly int m_hashSpotted = Animator.StringToHash("Spotted");
+        protected readonly int m_hashMeleeAttack = Animator.StringToHash("MeleeAttack");
+        protected readonly int m_hashShooting = Animator.StringToHash("Shooting");
+        protected readonly int m_hashTargetLost = Animator.StringToHash("TargetLost");
+        protected readonly int m_hashHit = Animator.StringToHash("Hit");
+        protected readonly int m_hashDeath = Animator.StringToHash("Death");
+        protected readonly int m_hashGounded = Animator.StringToHash("Grounded");
 
         // Start is called before the first frame update
         void Start() {
